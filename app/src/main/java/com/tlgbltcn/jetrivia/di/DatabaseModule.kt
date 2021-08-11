7f6c30e102ad8d4da63bf2616844daabc8aef78c
Tolga Bolatcan <tolga.bolatcan@adesso.com.tr>
2021-08-12 00:34:40 +0300

    hilt has been initialized.

---
+++ app/src/main/java/com/tlgbltcn/jetrivia/di/DatabaseModule.kt
@@ -0,0 +1,11 @@
+package com.tlgbltcn.jetrivia.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    // It will be provide.
+}
